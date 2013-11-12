8e2777f46b49df7ea5fb8cba576becb61e1fbcda
Weston Platter <westonplatter@gmail.com>
2013-11-12 00:18:52 -0700

    inject FG methods into rspec tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,13 +3,13 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'factory_girl_rails'
 
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
   config.include Mongoid::Matchers, type: :model
+  config.include FactoryGirl::Syntax::Methods, type: [:mode, :controller]
 
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
