545a857f9e0f53ead59aa6bd7be6e7b7fa1e633b
Weston Platter <westonplatter@gmail.com>
2013-12-24 21:52:21 -0700

    don’t include object declaration for child nodes

--- config/initializers/rabl.rb
+++ config/initializers/rabl.rb
@@ -1,6 +1,7 @@
 # https://github.com/nesquena/rabl#configuration
 
 require 'rabl'
+
 Rabl.configure do |config|
   # Commented as these are defaults
   # config.cache_all_output = false
@@ -12,12 +13,18 @@ Rabl.configure do |config|
   # config.msgpack_engine = nil # Defaults to ::MessagePack
   # config.bson_engine = nil # Defaults to ::BSON
   # config.plist_engine = nil # Defaults to ::Plist::Emit
+  
+  # config.include_json_root = true
   config.include_json_root = false
+  
   # config.include_msgpack_root = true
   # config.include_bson_root = true
   # config.include_plist_root = true
   # config.include_xml_root  = false
+  
   # config.include_child_root = true
+  config.include_child_root = false
+  
   # config.enable_json_callbacks = false
   # config.xml_options = { :dasherize  => true, :skip_types => false }
   # config.view_paths = []
